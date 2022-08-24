58f7ad64249d502fba431d8797ef2d671fb6fdc1
Gazali <gazali.ahmad@gmail.com>
2022-08-24 13:08:01 +0800

    Initial clean commit

---
+++ src/main/java/com/capstone/digitalStreamingSystemAPI/DigitalStreamingSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.digitalStreamingSystemAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalStreamingSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalStreamingSystemApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capstone/digitalStreamingSystemAPI/DigitalStreamingSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.digitalStreamingSystemAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalStreamingSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
