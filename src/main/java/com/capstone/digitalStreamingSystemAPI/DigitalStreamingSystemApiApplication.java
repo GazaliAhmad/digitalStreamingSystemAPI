cfdec538b6359d5d68a29ef8211d3872cc672a67
Gazali <gazali.ahmad@gmail.com>
2022-08-24 13:23:10 +0800

    application.properties define db done. CorsConfiguration added to DigitalStreamingSystemApiApplication Directory structure created

--- src/main/java/com/capstone/digitalStreamingSystemAPI/DigitalStreamingSystemApiApplication.java
+++ src/main/java/com/capstone/digitalStreamingSystemAPI/DigitalStreamingSystemApiApplication.java
@@ -1,7 +1,12 @@
 package com.capstone.digitalStreamingSystemAPI;
 
+import com.sun.istack.NotNull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DigitalStreamingSystemApiApplication {
@@ -9,5 +14,23 @@ public class DigitalStreamingSystemApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DigitalStreamingSystemApiApplication.class, args);
 	}
-
+	
+	
+	@Configuration
+	public static class CorsConfiguration {
+		@Bean
+		public WebMvcConfigurer corsConfigurer() {
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(@NotNull CorsRegistry registry) {
+					registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+					registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+					registry.addMapping("/**").allowedHeaders("*");
+					registry.addMapping("/**").exposedHeaders("*");
+					registry.addMapping("/**").allowCredentials(true);
+					registry.addMapping("/**").maxAge(3600);
+				}
+			};
+		}
+	}
 }
